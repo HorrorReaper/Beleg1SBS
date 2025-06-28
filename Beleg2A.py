ea2b48d3424bd411affa6b8d3cb2843801c00ddc
HorrorReaper <patrick.egi04@gmail.com>
2025-06-28 15:42:27 +0200

    Gerät besser eingebaut

--- Beleg2A.py
+++ Beleg2A.py
@@ -24,8 +24,9 @@ besucher_ankunftszeit_sigma = 1.5
 besucher_ankunftszeit_min = 0
 besucher_ankunftszeit_max = 10
 
-simulationsdauer = 840  # 1 Tag in Minuten
+simulationsdauer = 840  # 1 Tag in Minuten -> 8:00 Uhr bis 22:00 Uhr
 # gerät wartezeiten
+geraet_wartezeit = []
 geraet_nutzung_dauer_durchschnitt = 15  # Minuten
 geraet_nutzung_dauer_sigma = 5
 
@@ -35,16 +36,19 @@ service_times = []
 def intervalAnkunftszeit(env_now):
     if 480 <= env_now <= 600: #16-18 Uhr kommen die meisten Besucher
         return random.uniform(1, 4)
-    elif 660 <= env_now <= 720:
+    elif 600 < env_now <= 720:
         return random.uniform(1, 5)
+    elif 820 <= env_now <= 840: 
+        return 0 # keine neuen Besucher mehr 20 Minuten vor Schließung
     else:
         return random.uniform(4, 10)
 def Fitnessstudiobesucher(env, besucherid, trainer, typ, geraet):
     global wait_times, service_times
-    beginn_dauer = max(0, random.gauss(beginn_dauer_durchschnitt, beginn_dauer_sigma))
+    max_beginn_dauer = simulationsdauer - env.now - 1
+    beginn_dauer = min(max(0, random.gauss(beginn_dauer_durchschnitt, beginn_dauer_sigma)), max_beginn_dauer)
     print(int(env.now), 'Besucher', besucherid, ' betritt den Parkplatz %d' % int(env.now), 'Minuten nach Öffnung')
     yield env.timeout(beginn_dauer)
-    if env.now >= simulationsdauer:
+    if env.now >= simulationsdauer - 1:
         print(int(env.now), 'Besucher ', besucherid, ' verlässt das Fitnessstudio ', int(env.now), ' aufgrund von Schließung')
         return  # Besucher muss gehen
 
@@ -63,7 +67,8 @@ def Fitnessstudiobesucher(env, besucherid, trainer, typ, geraet):
             yield req
             wait = env.now - t_request
             wait_times.append(wait)
-            trainerzeit = max(0, random.gauss(trainerzeit_dauer_durchschnitt, trainerzeit_dauer_sigma))
+            max_trainerzeit = simulationsdauer - env.now - 1
+            trainerzeit = max(0,min(max(0, random.gauss(trainerzeit_dauer_durchschnitt, trainerzeit_dauer_sigma)), max_trainerzeit))
             verfuegbar = trainer.capacity - trainer.count
             print(int(env.now), "Trainer", trainer.count, " kümmert sich um Besucher ", besucherid, " um ", int(env.now), ". Es sind noch ", verfuegbar, " Trainer verfügbar. Es werden ", int(trainerzeit), " Minuten gewartet")
             
@@ -73,35 +78,39 @@ def Fitnessstudiobesucher(env, besucherid, trainer, typ, geraet):
             verfuegbar = trainer.capacity - trainer.count
             print(int(env.now), "Trainer", trainer.count, " hat die Hilfe für Besucher ", besucherid, " beendet. Es sind jetzt wieder ", verfuegbar, " Trainer verfügbar")
     if random.random() < 0.4:
+        t_request = env.now
         with geraet.request() as req:
             yield req
+            wait = env.now - t_request
+            geraet_wartezeit.append(wait)
             print(f"{int(env.now)} Besucher {besucherid} wartet auf Gerät bei Minute {env.now:.1f}")
-            dauer = max(0, random.gauss(geraet_nutzung_dauer_durchschnitt, geraet_nutzung_dauer_sigma))
+            max_geraet_nutzung_dauer = simulationsdauer - env.now - 1
+            dauer = max(0,min(max(0, random.gauss(geraet_nutzung_dauer_durchschnitt, geraet_nutzung_dauer_sigma)),max_geraet_nutzung_dauer))
             yield env.timeout(dauer)
             print(f"{int(env.now)} Besucher {besucherid} verlässt das Gerät bei Minute {env.now:.1f}")
 
-
-    trainings_dauer = max(0, random.gauss(trainings_dauer_durchschnitt, trainings_dauer_sigma))
+    max_trainings_dauer = simulationsdauer - env.now - 1
+    trainings_dauer = max(0,min(max(0, random.gauss(trainings_dauer_durchschnitt, trainings_dauer_sigma)), max_trainings_dauer))
     for t in range(int(env.now), int(env.now + trainings_dauer)):
         besucher_pro_minute[t] = besucher_pro_minute.get(t, 0) + 1
     print('Besucher', besucherid, ' betritt das Fitnessstudio, beginnt mit dem Training %d' % int(env.now), 'Minuten nach Öffnung')
     yield env.timeout(trainings_dauer)
-    if env.now >= simulationsdauer:
+    if env.now >= simulationsdauer - 1:
+        print(int(env.now), 'Besucher ', besucherid, ' verlässt das Fitnessstudio ', int(env.now), ' aufgrund von Schließung')
         return  # Besucher muss gehen
 
-
-    verlassen_duration = max(0, random.gauss(verlassen_dauer_durchschnitt, verlassen_dauer_sigma))
+    max_verlassen_dauer = simulationsdauer - env.now - 1
+    verlassen_duration = max(0,min(max(0, random.gauss(verlassen_dauer_durchschnitt, verlassen_dauer_sigma)), max_verlassen_dauer))
     print(int(env.now), 'Besucher ', besucherid,' verlässt das Fitnessstudio %d' % int(env.now))
     yield env.timeout(verlassen_duration)
-    if env.now >= simulationsdauer:
+    if env.now >= simulationsdauer - 1:
+        print(int(env.now), 'Besucher ', besucherid, ' verlässt das Fitnessstudio ', int(env.now), ' aufgrund von Schließung')
         return  # Besucher muss gehen
 
 
 def generiereFitnessstudiobesucher(env, trainer, geraet):
     besucherid = 0
-    while True:
-        if env.now > simulationsdauer - 20:
-            break  # keine neuen Besucher mehr ab 820
+    while env.now < simulationsdauer - 20:
 
         besucherid += 1
         print(int(env.now), "Besucher kommt an ", besucherid, " zur Zeit: ", int(env.now))
@@ -115,11 +124,12 @@ def generiereFitnessstudiobesucher(env, trainer, geraet):
         yield env.timeout(intervalAnkunftszeit(env.now))
 
 def run_scenario(num_trainers):
-    global wait_times, service_times, besucher_pro_minute, hilfe_pro_minute
+    global wait_times, service_times, besucher_pro_minute, hilfe_pro_minute, geraet_wartezeit
     wait_times = []
     service_times = []
     besucher_pro_minute = {}
     hilfe_pro_minute = {}
+    geraet_wartezeit = []
 
     env = simpy.Environment()
     trainer = simpy.Resource(env, capacity=num_trainers)
@@ -127,13 +137,14 @@ def run_scenario(num_trainers):
 
     env.process(generiereFitnessstudiobesucher(env, trainer, geraet))
     env.run(until=simulationsdauer)
-
+    avg_wait_geraet = np.mean(geraet_wartezeit) if geraet_wartezeit else 0.0
     avg_wait = np.mean(wait_times) if wait_times else 0.0
     max_wait = np.max(wait_times) if wait_times else 0.0
     utilization = sum(service_times) / (num_trainers * simulationsdauer) if num_trainers > 0 else 0
 
     return {
         'trainers': num_trainers,
+        'avg_wait_geraet': avg_wait_geraet,
         'avg_wait': avg_wait,
         'max_wait': max_wait,
         'utilization': utilization
@@ -181,5 +192,10 @@ plt.plot(hilfe_df['Minute'], hilfe_df['Anzahl Hilfe'], color='teal')
 plt.title("Anzahl Hilfe im Tagesverlauf")
 plt.xlabel("Minute seit Öffnung (8:00 Uhr)")
 plt.ylabel("Anzahl gleichzeitiger benötigter Hilfen")
+plt.subplot(4, 2, 6)
+plt.plot(df['trainers'], df['avg_wait_geraet'], color='teal')
+plt.title("Durchschnittliche Wartezeit am Gerät")
+plt.xlabel("Anzahl Trainer(zusammenhangslos)")
+plt.ylabel("Werte")
 plt.grid(True)
 plt.show()
